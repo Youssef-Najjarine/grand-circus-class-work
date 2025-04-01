0a634a4238b26d8d89963593bb3b0293520a1595
Youssef Najjarine <ynajjarine@gmail.com>
2025-03-31 21:24:13 -0500

    Completed AddNumbers assignment.

---
+++ Unit-2-Intro-To-C#/AddNumbers/AddNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace AddNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int entry = 0;
+            int sum = 0;
+            for (int i = 0; i < 3;i++)
+            {
+                Console.WriteLine("Please enter a number: ");
+                entry = int.Parse(Console.ReadLine());
+                sum += entry;
+            }
+            Console.WriteLine($"The sum is {sum}");
+
+        }
+    }
+}
