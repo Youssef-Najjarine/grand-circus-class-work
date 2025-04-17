033acf95a2ac69db7a31fb271085400ea0b17fb6
Youssef Najjarine <ynajjarine@gmail.com>
2025-04-16 20:45:37 -0500

    Changes made.

--- Unit-3-Collections/Day-2-For-Each-Example/Day-2-For-Each-Example/Program.cs
+++ Unit-3-Collections/Day-2-For-Each-Example/Day-2-For-Each-Example/Program.cs
@@ -5,7 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to an Example of a For-Each Loop");
-        
-        
+        // Ask the user to enter a sentence (series of words seperated by whitespace - space, tab, something you can't see)
+        // Display each word in the sentence
+
+        Console.WriteLine("Please enter a sentence");
+        string sentence = Console.ReadLine(); // Get what type and put it in a string
+
+        string[] splitSentence = sentence.Split(" "); // split the sentence into words
+
+        for (int i = 0; i < splitSentence.Length;i++)
+        {
+            Console.WriteLine(splitSentence[i]);
+        }
     }
 }
