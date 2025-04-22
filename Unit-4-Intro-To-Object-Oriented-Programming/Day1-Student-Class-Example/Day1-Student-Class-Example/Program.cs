7458201b43e570f873b9bd097083b089bb78b7a0
Youssef Najjarine <ynajjarine@gmail.com>
2025-04-21 21:00:24 -0500

    Changes.

---
+++ Unit-4-Intro-To-Object-Oriented-Programming/Day1-Student-Class-Example/Day1-Student-Class-Example/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Day1_Student_Class_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom to our student class example");
+            List<int> scores = new List<int>();
+            scores.Add(100);
+            scores.Add(90);
+            scores.Add(80);
+            Student aStudent = new Student("Youssef", scores);
+            Console.WriteLine($"aStudent: {aStudent}");
+            aStudent.ShowStudent();
+        }
+    }
+}
---
+++ Unit-4-Intro-To-Object-Oriented-Programming/Day1-Student-Class-Example/Day1-Student-Class-Example/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1_Student_Class_Example
+{
+    public class Student
+    {
+        private string studentName;
+        private List<int> testScores;
+
+        public Student(string name, List<int> scores)
+        {
+            studentName = name;
+            testScores = scores;
+        }
+        public void ShowStudent()
+        {
+            Console.WriteLine("Name: " + studentName);
+            Console.WriteLine("Scores: ");
+            foreach (int score in testScores)
+            {
+                Console.WriteLine(score);
+            }
+        }
+    }
+}
