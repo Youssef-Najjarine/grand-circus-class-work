306c58332983bec5001551524dae24683377a547
Youssef Najjarine <ynajjarine@gmail.com>
2025-05-31 14:41:11 -0500

    made changes

--- Unit-4-Intro-To-Object-Oriented-Programming/Enumerations_57-62/Enumerations_57-62/EllipsesShape.cs
+++ Unit-4-Intro-To-Object-Oriented-Programming/Enumerations_57-62/Enumerations_57-62/EllipsesShape.cs
@@ -6,23 +6,44 @@ using System.Threading.Tasks;
 
 namespace Enumerations_57_62
 {
-    internal abstract class EllipsesShape : Shape
+    internal class EllipsesShape : Shape
     {
         // constructors
-        protected EllipsesShape(string shapeName, int sides) : base(shapeName, sides)
+        protected EllipsesShape(string shapeName, int sides)
         {
             this.ShapeName = shapeName;
             this.SidesCount = sides;
         }
         // Properties
-        protected abstract double Radius { get; }
+        public string ShapeName { get; set; }
+        public int SidesCount { get; set; }
+        double Radius { get; }
+        public double Area { get; set; }
+        public double Perimeter { get; set; }
 
-
+        internal double CalculateArea()
+        {
+            return Math.Round(Math.PI * (this.Radius * this.Radius), 2);
+        }
+        internal double CalculatePerimeter()
+        {
+            return Math.Round(Math.PI * (2 * this.Radius), 2);
+        }
         // Methods
         public override string ToString()
         {
             return $"The {ShapeName} has a radius of {Radius}. The area is {Area} and the perimeter is {Perimeter}.";
 
+        }
+
+        double Shape.CalculateArea()
+        {
+            return CalculateArea();
+        }
+
+        double Shape.CalculatePerimeter()
+        {
+            return CalculatePerimeter();
         }
     }
 }
--- Unit-4-Intro-To-Object-Oriented-Programming/Enumerations_57-62/Enumerations_57-62/Shape.cs
+++ Unit-4-Intro-To-Object-Oriented-Programming/Enumerations_57-62/Enumerations_57-62/Shape.cs
@@ -8,27 +8,14 @@ namespace Enumerations_57_62
 {
     internal interface Shape
     {
-        // Fields
-        //protected string _shapeName;
-        //protected int _sideCount;
-        //protected double _area;
-        //protected double _perimeter;
-
-        //// Constructors
-        //protected Shape(string shapeName, int sides)
-        //{
-        //    this._shapeName = shapeName;
-        //    this._sideCount = sides;
-        //}
-
         // Properties
-        protected string ShapeName { get; set; }
-        protected int SidesCount { set; }
-        public double Area { get; set; }
-        public double Perimeter { get; set; }
+        string ShapeName { get; set; }
+        int SidesCount { set; }
+        double Area { get; set; }
+        double Perimeter { get; set; }
 
         // Methods
-        protected double CalculateArea();
-        protected double CalculatePerimeter();
+        double CalculateArea();
+        double CalculatePerimeter();
     }
 }
