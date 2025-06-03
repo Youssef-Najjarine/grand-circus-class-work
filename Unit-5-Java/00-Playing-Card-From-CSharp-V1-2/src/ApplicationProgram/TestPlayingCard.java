563a184b6cf786c0f98fb11cedc2f4efd2d47929
Youssef Najjarine <ynajjarine@gmail.com>
2025-06-02 19:26:21 -0500

    Completed task.

--- Unit-5-Java/00-Playing-Card-From-CSharp-V1-2/src/ApplicationProgram/TestPlayingCard.java
+++ Unit-5-Java/00-Playing-Card-From-CSharp-V1-2/src/ApplicationProgram/TestPlayingCard.java
@@ -1,9 +1,14 @@
 package ApplicationProgram;
 
+import PlayingCard.PlayingCard;
+
 public class TestPlayingCard {
     public static void main(String[] args) {
 
         System.out.println("Hello, World!");
-
+        PlayingCard aceOfSpades = new PlayingCard(PlayingCard.CardValue.Ace, PlayingCard.CardSuit.Spade);
+        System.out.println(aceOfSpades);
+        aceOfSpades.Flip();
+        System.out.println((aceOfSpades));
     } // End of main() method
 } // End of TestPlayingCard CLass
--- Unit-5-Java/00-Playing-Card-From-CSharp-V1-2/src/PlayingCard/PlayingCard.java
+++ Unit-5-Java/00-Playing-Card-From-CSharp-V1-2/src/PlayingCard/PlayingCard.java
@@ -1,15 +1,14 @@
-﻿// namespace Playing_Card_Poker_Game;  // C# groups things in namespaces
+// namespace Playing_Card_Poker_Game;  // C# groups things in namespaces
 package PlayingCard; // Java groups things in package - package name must match the folder name for the package
+
+import java.util.Objects;
 
 public class PlayingCard
 {
     /***************************************************************************************************
-     * Define constants to represent card attributes
-     *
+     * Define constants to represent card attribute
      * public is OK since they are constants and cannot be changed
-     *
      * static so it can be referenced using the class name. i.e. no object required
-     *
      * enum - define a set of constant values that may be referenced as a data type
      *        allows the assign of a word to a constant value to limit the values in a variable
      *        make it easier to code (get rid using "magic" numbers to represent data
@@ -23,29 +22,29 @@ public class PlayingCard
     {  // define words to represent allowable card colors (instead of String)
         // 0  , 1  
         Black, Red                 // These are the only valid values C# will allow
-    };
+    }
 
     public enum CardSuit
     {   // public is OK since they are constants and cannot be changed
         Spade, Club, Heart, Diamond, Joker  // These are the only valid values C# will allow
-    };
+    }
 
     public enum CardValue
     {  // Using the fact that enums are really integers inside value to name our values
         //  0    1    2     3      4    5     6     7      8      9    10   11    12     13    14
         Joker, One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace
-    };
+    }
 
     /***************************************************************************************************
      * Define constants to represent defaults for card attributes
-     *
+
      * protected means members of this class and members of any subclass can access the data / method
      *           directly without having to use methods to do so
-     *
+
      * protected so subclasses may access is OK since they are constants and cannot be changed
-     *
+
      * static so it can be referenced using the class name. i.e. no object required
-     *
+
      *   C#: readonly so it can't be changed - const is Ok too 
      * Java: final so it can't be changed
      ***************************************************************************************************/
@@ -55,9 +54,9 @@ public class PlayingCard
     protected static final CardSuit  DEFAULTSUIT      = CardSuit.Joker;
     /***************************************************************************************************
      * Member data
-     *
+
      * private to protect as prescribed by encapsulation - method must be used to access the data 
-     * 
+
      *   C#: private variable name starts with _ by convention
      * Java: has no convention for naming private variables
      ***************************************************************************************************/
@@ -95,7 +94,7 @@ public class PlayingCard
      ***************************************************************************************************/
     public PlayingCard(int value, CardSuit suit)
     {
-        this.value = SetValue(value); // Set value in object based on int value passed as argument
+        this.value = setValue(value); // Set value in object based on int value passed as argument
         SetColor(suit);               // Set color in object based on suit passed as argument
         this.suit = suit;             // Set suit in object to suit passed as argument
         faceUp = false;
@@ -104,7 +103,6 @@ public class PlayingCard
      * C# Only: Properties - Used for others to access private data via getters and setters
      * Java does not support properties
      * Java requires specific getter and setter methods for each private variables
-     **************************************************************************************************/
     /***************************************************************************************************
      * Standard  getter and setter methods
      ***************************************************************************************************/
@@ -147,10 +145,10 @@ public class PlayingCard
     // Return the integer value assigned to the card - value is an enum called CardValue
     public int GetIntValue()
     {   // Return integer value of CardValue enum in a PlayingCard 
-        return (int) value;  // cast the enum to an int to get its integer value
+        return value.ordinal();  // cast the enum to an int to get its integer value
     }
 
-    public CardValue SetValue(int ivalue)
+    public CardValue setValue(int ivalue)
     {  // Set the CardValue based on an int value
         /***********************************************************************************************************
          * switch is alternative to a a set of nested if-then-else-if statements
@@ -221,7 +219,6 @@ public class PlayingCard
                 break;      // break is required so we don't fall through to the next case - we exit the switch
             default:
                 this.color = DEFAULTCOLOR;  // break is needed here even through its last case
-                break;
         }
     }
 
@@ -238,55 +235,39 @@ public class PlayingCard
      *      GetHashCode() - generate hash code based on unchanging data members
      * 
      ***************************************************************************************************/
-    // Ask compiler to assure this is a valid super class override
-    public override string ToString()
-    { // Return a String representation of the object
-        return $"Playing Card: Value={_value}({(int)_value})\tSuit={_suit}\tColor={_color}\tFace Up={_faceUp}";
+    @Override
+    public String toString() {
+        return "playingCard{" +
+                "value=" + value +
+                ", color=" + color +
+                ", suit=" + suit +
+                ", faceUp=" + faceUp +
+                '}';
     }
 
-    // Ask compiler to assure this is a valid super class override
-    public override bool Equals(object otherObject)
-    {   // Compare Two PlayingCards for equality - note generic Object parameter
-    
-        if (otherObject == this)
-        {   // if object being compared to itself
-            return true;             // objects are equal
-        }
-        else
-        {
-            // 'is' returns true if an object is an instance of the class specified
-            if ((otherObject is PlayingCard))
-            {   // If the object being compared to is the same class as object 
-                PlayingCard otherCard = (PlayingCard)otherObject; // Define a PlayingCard object from object being compared to
-                return (this.value == otherCard.value && this.color == otherCard.color && this.suit == otherCard.suit);
-            }
-            return false;    // Required so IDE realizes all logic paths are closed
-        }
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        PlayingCard that = (PlayingCard) o;
+        return faceUp == that.faceUp && value == that.value && color == that.color && suit == that.suit;
     }
-    // Ask compiler to assure this is a valid super class override
-    public override int GetHashCode()
-    {   // Generate a hash Code for object if C# needs One e.g. for a HashMap
-        // A hash code is a unique value representing an instance of an object
-        // It is recommended to use member variables whose value is unlikely to change
-        //
-        // The HashCode class has a method called .Combine()
-        //     which will generate a hash code from the values in the variable you send it
-        //
-        // The same variables used to determine object equality should be used
-        //     when generating a hash code
-        return HashCode.Combine(value, suit, color);
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, color, suit, faceUp);
     }
 
     /***************************************************************************************************
      * Miscellaneous class methods
      ***************************************************************************************************/
-    public void ShowCard()
+    public void showCard()
     {   // Display an instance of a PlayingCard
-        Console.WriteLine(this.ToString());
+//        Console.WriteLine(this.ToString());
+        System.out.println(this.toString());
     }
 
     public void ShowCardWithHash()
     {   // Display an instance of a PlayingCard with HashCode
-        Console.WriteLine(this.ToString() + "\thashCode: " + GetHashCode());
+        System.out.println(this.toString() + "\thashCode: " + hashCode());
     }
 }  // End of PlayingCard Class
