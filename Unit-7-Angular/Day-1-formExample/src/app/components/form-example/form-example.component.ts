6c88ad4e584d8511f07d63d16eabb5e1077879d1
Youssef Najjarine <ynajjarine@gmail.com>
2025-06-25 21:36:45 -0500

    WEdnesday.

--- Unit-7-Angular/Day-1-formExample/src/app/components/form-example/form-example.component.ts
+++ Unit-7-Angular/Day-1-formExample/src/app/components/form-example/form-example.component.ts
@@ -11,9 +11,20 @@ import { throwError } from 'rxjs';
 })
 // Allow access to stuff defined this component from outside the component
 export class FormExampleComponent {
-  buttonClicked = false
+  buttonClicked = false;
+  firstName = "";
+  lastName = "";
+  birthDate = "";
+  foodPreference = "";
+  emailAddress = "";
+  emailPreference = "";
+  sports = ""
+
   onSubmit() { // Handle the Submit button click on the  form
     this.buttonClicked = true
     console.log(`OK.... You Clicked Submit`)
+    console.log(`You entered ${this.firstName} in the First Name field`);
+    console.log(`You entered ${this.emailPreference} ${this.sports}`);
+
   }
 }
